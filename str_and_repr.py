1be28821d1c3650b78481974e4445b14a70a4f06
Daniel Tomaszuk <dtomaszuk01@gmail.com>
2024-06-10 13:49:52 +0200

    Add __format__ examples

--- str_and_repr.py
+++ str_and_repr.py
@@ -19,7 +19,19 @@ class Taco:
     def __str__(self) -> str:
         # Informal representation of an instance, fallbacks to __repr__ if not defined
         toppings: str = (f"with {", ".join(self.toppings)}" if self.toppings else "")
-        return f"{self.shell} shell {self.protein} taco {toppings} @ {id(self)}"
+        return f"{self.shell} shell {self.protein} taco {toppings}"
+
+    def __format__(self, format_spec: str) -> str:
+        print("Format spec:", format_spec)
+
+        if format_spec.endswith("d"):
+            return str(id(self))
+
+        # custom formatting string
+        if format_spec.endswith("taco"):
+            return self.__repr__().upper() + "!!!!"
+
+        return super().__format__(format_spec)
 
 
 if __name__ == "__main__":
@@ -31,3 +43,14 @@ if __name__ == "__main__":
 
     print(repr(t1))
     print(repr(t2))
+
+    # format doesn't call t1.__format__  if it knows how to format the class on its own
+    print("Format as string:", "{!s}".format(t1))
+    print("Format as repr:", "{!r}".format(t1))
+
+    # t1.__format__  is called here, default formatting isn't  known so instance method is called
+    print("{}".format(t1))
+
+    # some additional formatting with format strings
+    print("{:02d}".format(t1))
+    print("{:taco}".format(t1))  # not really a good idea, but will work as long as Taco class supports it
